bfd6615b46902420d85bf277a59441114e5a0564
Joshua Powell <joshua@viableindustries.com>
2013-11-25 12:31:06 -0500

    Added all contaminant popovers;

--- _site/static/js/application.js
+++ _site/static/js/application.js
@@ -1,5 +1,19 @@
 (function(jQuery) {
 
   "use strict";
+  
+  var init_app = function () {
+    
+    jQuery('.element, .close-element').each(function () {
+      jQuery(this).click(function () {
+        console.log(jQuery(this).data('toggle'), ' was clicked');
+        jQuery('#' + jQuery(this).data('toggle')).toggleClass('visible');
+        jQuery('html,body').animate({scrollTop: 0}, 100);
+      });
+    });
+    
+  };
+  
+  jQuery(document).ready(init_app);
 
 })(jQuery);
