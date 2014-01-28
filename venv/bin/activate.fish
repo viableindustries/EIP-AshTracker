d42a43b3622a2d9669a8cda95bf97fe31dbdcfe5
Joshua Powell <joshua@viableindustries.com>
2014-01-28 13:14:05 -0500

    Updated the home page content; Added Google Analytics code to the default template;

--- venv/bin/activate.fish
+++ venv/bin/activate.fish
@@ -32,7 +32,7 @@ end
 # unset irrelevant variables
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/Users/joshuaisaacpowell/Desktop/ashtracker/venv"
+set -gx VIRTUAL_ENV "/Users/joshuaisaacpowell/Code/ashtracker/venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
