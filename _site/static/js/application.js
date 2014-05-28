dc365c35b9887db50e75101a432e8a86f5947d03
brendandm <brendanmcintyre@Brendans-MacBook-Pro.local>
2014-05-28 08:00:45 -0400

    Rebuilt site for EIP release

--- _site/static/js/application.js
+++ _site/static/js/application.js
@@ -4,7 +4,7 @@
   
   var init_app = function () {
     
-    jQuery('.element, .close-element').each(function () {
+    jQuery('.element-clickable, .close-element').each(function () {
       jQuery(this).click(function () {
         console.log(jQuery(this).data('toggle'), ' was clicked');
         jQuery('#' + jQuery(this).data('toggle')).toggleClass('visible');
@@ -12,11 +12,11 @@
       });
     });
     
-    jQuery('.element.not-tracked, .element.exceeded').tooltip();
+    //jQuery('.element.not-tracked, .element.exceeded, .element.compliant').tooltip();
     
-    jQuery('.element.element-clickable.violation').hover(function () {
-      jQuery(this).tooltip('toggle');
-    });
+    //jQuery('.element.element-clickable.violation').hover(function () {
+      //jQuery(this).tooltip('toggle');
+    //});
     
     jQuery('.map-area').click(function() {
       
@@ -46,6 +46,12 @@
       jQuery(".map-area").show();
       
       jQuery(".info_area").hide();
+    });
+
+    jQuery('.wells').DataTable({
+        searching: false,
+        info: false,
+        order: [[1, "desc"]]
     });
     
   };
